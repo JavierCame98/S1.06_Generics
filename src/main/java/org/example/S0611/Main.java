87153f59575debdcd589902816f4eaa9dd4fb295
Javier Castillo <javiercame98@gmail.com>
2025-12-11 11:26:15 +0100

    feat: creating Main class to check no generic method

--- src/main/java/org/example/S0611/Main.java
+++ src/main/java/org/example/S0611/Main.java
@@ -1,4 +1,27 @@
 package org.example.S0611;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("##Checking that the arguments can be put in a different order");
+
+        NoGenericMethods ngm1 = new NoGenericMethods("Albacete", "Barcelona", "Cáceres");
+        System.out.println(ngm1);
+        NoGenericMethods ngm2 = new NoGenericMethods("Cáceres", "Albacete", "Barcelona");
+        System.out.println(ngm2);
+
+        System.out.println("##Checking setters and getters");
+
+        ngm1.setFirst("Madrid");
+        ngm1.setSecond("Oviedo");
+        ngm1.setThird("Sevilla");
+        System.out.println(ngm1);
+
+        System.out.println("First city: " + ngm1.getElement1());
+        System.out.println("Second city: " + ngm1.getElement2());
+        System.out.println("Third city: " + ngm1.getElement3());
+
+
+    }
 }
